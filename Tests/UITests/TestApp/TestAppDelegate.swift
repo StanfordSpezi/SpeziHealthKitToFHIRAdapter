e43f2c05cebcd16a697397721e6a2d72bf418a9c
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-05-18 11:07:11 -0700

    Updates the Project to be Part of the Spezi Organization (#6)

--- Tests/UITests/TestApp/TestAppDelegate.swift
+++ Tests/UITests/TestApp/TestAppDelegate.swift
@@ -1,20 +1,20 @@
 //
-// This source file is part of the CardinalKit open-source project
+// This source file is part of the Stanford Spezi open-source project.
 //
 // SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
 //
 // SPDX-License-Identifier: MIT
 //
 
-import CardinalKit
-import CardinalKitFHIR
-import CardinalKitHealthKit
-import CardinalKitHealthKitToFHIRAdapter
 @preconcurrency import HealthKit
+import Spezi
+import SpeziFHIR
+import SpeziHealthKit
+import SpeziHealthKitToFHIRAdapter
 import SwiftUI
 
 
-class TestAppDelegate: CardinalKitAppDelegate {
+class TestAppDelegate: SpeziAppDelegate {
     override var configuration: Configuration {
         Configuration(standard: FHIR()) {
             if HKHealthStore.isHealthDataAvailable() {
--- Tests/UITests/TestAppUITests/TestAppUITests.swift
+++ Tests/UITests/TestAppUITests/TestAppUITests.swift
@@ -1,5 +1,5 @@
 //
-// This source file is part of the CardinalKit open-source project
+// This source file is part of the Stanford Spezi open-source project.
 //
 // SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
 //
@@ -19,7 +19,7 @@ class TestAppUITests: XCTestCase {
     }
     
     
-    func testCardinalKitHealthKitToFHIRAdapterWithECG() throws {
+    func testSpeziHealthKitToFHIRAdapterWithECG() throws {
         let app = XCUIApplication()
         app.deleteAndLaunch(withSpringboardAppName: "TestApp")
         
